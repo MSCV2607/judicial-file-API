851da573e25418a67d3c235cc72028adfae3cf88
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-05-15 19:21:38 -0300

    Inicializacion del proyecto

---
+++ src/test/java/com/grupoNoctua/judicial_file_API/JudicialFileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoNoctua.judicial_file_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudicialFileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
