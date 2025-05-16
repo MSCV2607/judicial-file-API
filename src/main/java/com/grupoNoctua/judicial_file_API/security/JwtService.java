3ae3ed26452b2bf6503de7af048bcee9e474ec84
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-05-16 11:37:19 -0300

    Configurando JWT

--- src/main/java/com/grupoNoctua/judicial_file_API/security/JwtService.java
+++ src/main/java/com/grupoNoctua/judicial_file_API/security/JwtService.java
@@ -1,4 +1,70 @@
 package com.grupoNoctua.judicial_file_API.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+
+    //Clave secreta en Base64 (mínimo 256 bits si usás HS256)
+    private static final String SECRET_KEY = "6F6D61636C6176656D617375706572736563726574616A7774313233";
+
+    //Tiempo de expiración: 24 horas
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
+
+    //Genera un token con el username como "subject"
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //Valida que el token sea válido y pertenezca a ese usuario
+    public boolean isTokenValid(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username)) && !isTokenExpired(token);
+    }
+
+    //Extrae el username (subject) del token
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    //Extrae un claim personalizado
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    //Devuelve true si el token ya venció
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    //Devuelve todos los claims desde el token
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    //Devuelve la clave secreta como objeto Key
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY); // decodifica el string
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
